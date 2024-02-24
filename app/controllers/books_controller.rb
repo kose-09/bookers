1a58c3dcd0911c4bebcb8ed23d0d47432530dd6e
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-02-24 15:03:53 +0000

    [Add] 一覧画面,詳細画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,10 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -15,7 +17,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/'
+    redirect_to book_path(book.id)
   end
 private
  def book_params
